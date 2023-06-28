7d419cb2cf0003bd465bd94b7955f7a9f17df55c
kelsit0 <ramosreynagaoscar6@gmail.com>
2023-06-28 12:38:45 -0600

    add web server and update requeriments.txt

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,15 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,4]
+
+@app.get('/contact')
+def get_list():
+    return {'contact':'ejemplo@gmail.com'}
 
 def run():
     store.get_categories()
